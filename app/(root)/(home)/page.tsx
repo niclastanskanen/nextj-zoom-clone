0953d9afa9b47c45a850c025c190323596c72f58
Niclas <niclastanskanen@hotmail.com>
2024-04-02 14:55:10 +0200

    Add MeetingTypeList component to Home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
   const now = new Date();
@@ -23,6 +23,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <MeetingTypeList />
     </section>
   )
 }
