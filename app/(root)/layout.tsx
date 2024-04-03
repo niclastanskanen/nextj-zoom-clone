938b660bfaebc9a7a9ca310361a51d7b596ec3a3
Niclas <niclastanskanen@hotmail.com>
2024-04-03 13:33:52 +0200

    Add metadata for YOOM app

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,15 @@
 import React, { ReactNode } from 'react'
+import { Metadata } from 'next';
 
 import StreamVideoProvider from '@/providers/StreamClientProvider'
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video Calling App",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
